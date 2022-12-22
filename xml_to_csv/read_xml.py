ddc47219fe482894faef3b49febd810c594e6789
t-bowcock <tyler.j.bowock@gmail.com>
2022-12-22 16:43:23 +0000

    reads item xml data to json file

--- xml_to_csv/read_xml.py
+++ xml_to_csv/read_xml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 import re
+import pandas as pd
 from bs4 import BeautifulSoup
 
 item_regex = re.compile("content =(.*?)}}", flags=re.DOTALL)
@@ -14,13 +15,49 @@ item_notes_regex = re.compile(r"==\s?Notes\s?==\n+(.*?)\n\n", flags=re.DOTALL)
 item_synergies_regex = re.compile(r"==\s?Synergies\s?==\n+(.*?)\n\n", flags=re.DOTALL)
 item_interactions_regex = re.compile(r"==\s?Interactions\s?==\n+(.*?)\n\n", flags=re.DOTALL)
 
-list_regex = re.compile(r"\*+(.*?)\*", flags=re.DOTALL)
+bullet_regex = re.compile(r"(\*+)")
 
 
 def get_soup(path: str) -> BeautifulSoup:
     with open(path, encoding="utf-8", mode="r") as f:
         xml = f.read()
     return BeautifulSoup(xml, "xml")
+
+
+def dig(nested, depth):
+    for _ in range(0, (depth - 1)):
+        nested = nested[-1]
+    return nested
+
+
+def parse_list(list_string: str) -> list:
+    split_list = bullet_regex.split(list_string)
+    output = []
+    previous = 0
+    one_star_counter = -1
+    for i in range(1, len(split_list), 2):
+        new = len(split_list[i])
+        line = split_list[i + 1].strip()
+        if new == 1:
+            one_star_counter += 1
+            output.append([line])
+        else:
+            if new == previous:
+                dig(output[one_star_counter], previous).append(line)
+            elif new > previous:
+                dig(output[one_star_counter], previous).append([line])
+            elif new < previous:
+                dig(output[one_star_counter], new).append(line)
+        previous = new
+    return output
+
+
+def parse_string(string: str) -> str:
+    # TODO remove square brackets
+    # TODO turn dlc tags to text
+    # TODO decide how relationships are handled
+    # TODO parse any html (seen some <br> tags)
+    pass
 
 
 def get_all_items(path: str):
@@ -79,10 +116,10 @@ def get_all_items(path: str):
                 "description": item_description,
                 "quality": item_quality,
                 "unlock": item_unlock,
-                "effects": item_effects,
-                "notes": item_notes,
-                "synergies": item_synergies,
-                "interactions": item_interactions,
+                "effects": parse_list(item_effects) if item_effects is not None else None,
+                "notes": parse_list(item_notes) if item_notes is not None else None,
+                "synergies": parse_list(item_synergies) if item_synergies is not None else None,
+                "interactions": parse_list(item_interactions) if item_interactions is not None else None,
             }
     with open("items.json", encoding="utf-8", mode="w") as output:
         json.dump(item_dict, output)
